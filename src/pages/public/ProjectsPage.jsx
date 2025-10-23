baca1943a556d001458b1ec454df8fb914a3f05c
carolhz <150878380+carolhz@users.noreply.github.com>
2025-10-23 15:01:19 +0700

    Fix project data fetching in ProjectsPage component
    Updated project data fetching to use 'results' array.

--- src/pages/public/ProjectsPage.jsx
+++ src/pages/public/ProjectsPage.jsx
@@ -1,16 +1,15 @@
 // src/pages/public/ProjectsPage.jsx
 import React, { useState, useEffect } from 'react';
 import api from '../../lib/api';
-import ProjectCard from '../../components/ProjectCard'; // Impor card kita
+import ProjectCard from '../../components/ProjectCard'; 
 import { motion } from 'framer-motion';
 
-// Varian animasi untuk 'pembungkus' grid
 const gridContainerVariants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.1, // Animasi 'stagger' (muncul satu per satu)
+      staggerChildren: 0.1, 
     },
   },
 };
@@ -22,9 +21,12 @@ const ProjectsPage = () => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        // Ambil data dari /api/projects/ (sudah diurutkan by ID di Django)
         const response = await api.get('/projects/');
-        setProjects(response.data);
+        
+        // --- INI PERBAIKANNYA ---
+        setProjects(response.data.results); // Ambil array 'results' dari data
+        // -------------------------
+
       } catch (error) {
         console.error("Gagal mengambil data proyek:", error);
       } finally {
@@ -45,18 +47,12 @@ const ProjectsPage = () => {
         Proyek <span className="text-primary-pink">Saya</span>
       </h1>
 
-      {/* Grid Container dengan Animasi */}
       <motion.div
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
         variants={gridContainerVariants}
         initial="hidden"
         animate="visible"
       >
-        {/* Setiap ProjectCard adalah 'child' dari motion.div.
-          Karena parent-nya punya 'staggerChildren', 
-          dan ProjectCard punya varian 'hidden' & 'visible' (dari file-nya sendiri),
-          mereka akan otomatis muncul satu per satu.
-        */}
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
